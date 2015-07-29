ccd8d90018377f76114ad3288a0f958348c17508
phiresky <phiresky@users.noreply.github.com>
2015-07-29 05:11:10 +0200

    also load AUR

--- download.sh
+++ download.sh
@@ -9,3 +9,18 @@ for f in core extra community multilib; do
 	tar xf data/$fname -C $out
 	rm data/$fname
 done
+
+# aur
+(
+	cd data
+	[ -d 'aur-mirror' ] || git clone --depth=1 git://pkgbuild.com/aur-mirror.git
+	cd aur-mirror
+
+	function runmksrc() {
+		[ -f "$1/.SRCINFO" ] || mksrcinfo -o "$1/.SRCINFO" "$1/PKGBUILD"
+	}
+
+	export -f runmksrc
+
+	parallel -j8 --bar runmksrc ::: *
+)
